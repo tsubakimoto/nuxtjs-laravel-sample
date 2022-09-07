ac0610a2a8fd7bc2f8a4eeb04d3a92d61790c7be
Yuta Matsumura <y-matsumura@alterbooth.com>
2022-09-07 01:24:03 +0000

    Create nuxt app

---
+++ sample/nuxt-sample/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'no-useless-constructor': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'no-use-before-define': 'off',
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: false,
+      },
+    ],
+  },
+}
---
+++ sample/nuxt-sample/nuxt.config.js
@@ -0,0 +1,58 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-sample',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+
+    // https://www.npmjs.com/package/@nuxtjs/proxy
+    '@nuxtjs/proxy', // <= 追加
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    proxy: true, // <= 修正
+  },
+
+  proxy: { // <= 追加
+    '/api': 'http://localhost:8000', // <= 追加
+  }, // <= 追加
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
